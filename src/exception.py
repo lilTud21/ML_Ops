a48c76d3c8be396b16beb8ca7496d8fb90a94c4e
lilTud21 <rohitedwintudu19@gmail.com>
2025-02-01 22:10:34 +0530

    logging and exception

--- src/exception.py
+++ src/exception.py
@@ -1,16 +1,18 @@
 import sys
+import logging
 
 def error_message_detail(error,error_deatil:sys):
     _,_,exc_tb=error_detail.exc_info()
     file_name=exc_tb.tb_frame.f_code.co_filename
     error_message="Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(
-        file_name,exc_tb.tb_lineo,str(error)
-     return error_message
-    )
+        file_name,exc_tb.tb_lineo,str(error))
+    
+    return error_message
+    
     
 class CustomException(Exception):
     def _innit_(self,error_message,error_detail:sys):
-        super._innit_(error_message)
+        super()._innit_(error_message)
         self.error_message=error_message_detail(error_message,error_deatil=error_detail)
 
     def _str_(self):
